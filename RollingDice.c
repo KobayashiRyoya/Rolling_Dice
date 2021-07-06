b6afbb65f0e7e3b57b768b83a3fa31d4a3c1bdcf
小林亮也 <kobayashiryoya@wlan-napt-001.u-aizu.ac.jp>
2021-07-06 18:28:13 +0900

    Initial revision

---
+++ RollingDice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+int main(){
+	int i,total=0,Total;
+	srand((unsigned int)time(NULL));
+
+	printf("Rolling the dice...\n");
+
+	for(i=0;i<2;i++){
+		printf("Die%d:",i+1);
+		printf("%d\n",Total=rand()%6+1);
+		total=total+Total;
+	}
+		printf("total:%d\n",total);
+}
